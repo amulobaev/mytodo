35916be6160e853976e0dc88e4c2b2bb104202e3
Антон Мулобаев <devnull@localhost>
2015-05-28 13:04:36 +0500

    Сделал загрузку списка задач через ajax, добавил Font Awesome

---
+++ MyToDo/Controllers/TasksController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using MyToDo.Core;
+using MyToDo.Data;
+
+namespace MyToDo.Controllers
+{
+    [Authorize]
+    public class TasksController : ApiController
+    {
+        private readonly IRepository<MyTask> _repository;
+
+        public TasksController(ICustomRepository<MyTask> repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<MyTask> GetAllTasks()
+        {
+            var userId = User.Identity.GetUserId();
+            var tasks = _repository.GetAll().ToList();
+            return tasks;
+        }
+    }
+}
